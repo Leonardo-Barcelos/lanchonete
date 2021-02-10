24c4163766ce7f0b5eed159211de9091f9f75283
Leonardo Barcelos <d3pro3@outlook.com>
2021-02-10 11:55:29 -0300

    Reestruturação Projeto

--- app_lanchonete/lib/main.dart
+++ app_lanchonete/lib/main.dart
@@ -1,30 +1,5 @@
-import 'package:app_lanchonete/screens/auth_screen.dart';
-import 'package:app_lanchonete/screens/signup_screen.dart';
-import 'package:app_lanchonete/util/app_constants.dart';
-import 'package:app_lanchonete/util/app_routes.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  ThemeData _myThemeData = new ThemeData(
-    primarySwatch: Colors.lightBlue,
-    primaryTextTheme: TextTheme(
-      headline6: TextStyle(color: Colors.white),
-    ),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppConstants.APP_NAME,
-      theme: _myThemeData,
-      routes: {
-        AppRoutes.AUTH_HOME: (ctx) => AuthScreen(),
-        AppRoutes.AUTH_SIGNUP: (ctx) => SignupScreen(),
-      },
+void main() => runApp(
+      Container(),
     );
-  }
-}
--- app_lanchonete/lib/util/app_routes.dart
+++
@@ -1,4 +0,0 @@
-class AppRoutes {
-  static const AUTH_HOME = '/';
-  static const AUTH_SIGNUP = 'user-signup';
-}
